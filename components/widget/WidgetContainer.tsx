e63fa27033dcb0141f2e10c3614f3d03052d93b8
aortiz13 <serviciosmensualesadrian@gmail.com>
2026-02-19 20:25:32 -0300

    style: adjust developer attribution position

--- components/widget/WidgetContainer.tsx
+++ components/widget/WidgetContainer.tsx
@@ -1424,7 +1424,7 @@ export default function WidgetContainer({
             </main>
 
             {/* Developer Attribution - Always Visible */}
-            <div className="absolute bottom-6 right-8 z-30 opacity-60 pointer-events-none select-none">
+            <div className="absolute bottom-2 right-4 z-30 opacity-60 pointer-events-none select-none">
                 <span className="text-[9px] uppercase tracking-[0.1em] text-zinc-400 dark:text-zinc-500 font-medium">
                     Desarrollado por Judez-Logic
                 </span>
